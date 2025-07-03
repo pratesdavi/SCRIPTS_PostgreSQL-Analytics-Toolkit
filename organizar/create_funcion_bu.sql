77c5a2cb045756a820687d0ea8393fead919e605
Davi Prates <eudaviprates@gmail.com>
2025-07-03 10:06:55 -0300

    att

---
+++ organizar/create_funcion_bu.sql
@@ -0,0 +1,39 @@
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
+
+update f_validacao_vendas fv
+set ite_id = fv.ite_id 
+from f_vendas f
+where f.codigo_agrupador = fv.codigo_agrupador;
